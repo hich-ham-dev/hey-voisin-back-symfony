c2e07c156dcb4a654f4f1bd5a5fd14e1ec3e67ab
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-09-22 13:55:19 +0200

    add_users_controller

---
+++ src/Form/UsersType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('alias')
+            ->add('email')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('localities')
+            ->add('roles')
+            ->add('avatars')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
