726293ea159b3eced9836be16f356c7dbb1edbbc
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-09-22 14:45:47 +0200

    WIP_fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker; 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
-{
+{   
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher; 
+    }
+
+    /**
+     * Fonction qui va s'exécuter quand on va charger les fixtures (envoyer les données en BDD)
+     *
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Instanciation de Faker avec localisation en français
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $admin = new Users;
+        $admin->setAlias($faker->alias());
+        $admin->setEmail("admin@gmail.com");
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, "admin"));
+        $admin->setFirstname($faker->firstName());
+        $admin->setLastname($faker->lastName());
+        $admin->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
