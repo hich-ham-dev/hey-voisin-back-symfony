012aec8a0e38584ba1bf708ad5f64a37cc288cfd
jérémy <jeremy.jqt123@gmail.com>
2023-09-26 09:30:30 +0200

    add_prefixes_category

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -9,17 +9,18 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api')]
 class CategoryController extends AbstractController
 {
-    #[Route('/api/category', name: 'app_api_category')]
+    #[Route('/category', name: 'app_api_category', methods: ['GET'])]
     public function index(CategoryRepository $category): JsonResponse
     {
         $categories = $category->findAll();
 
         return $this->json($categories, Response::HTTP_OK, [], ['groups' => 'categories']);
     }
-    
-    #[Route('/api/category/{id}', name: 'app_api_post_show')]
+
+    #[Route('/category/{id}', name: 'app_api_post_show' , methods: ['GET'])]
     public function show(CategoryRepository $category, int $id): JsonResponse
     {
         $category = $category->find($id);
