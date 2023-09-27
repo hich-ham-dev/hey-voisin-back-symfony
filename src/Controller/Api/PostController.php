c1371e3812b59c4b0933c396f679656128e8ce80
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-09-27 17:01:36 +0200

    wip_post_new

--- src/Controller/Api/PostController.php
+++ src/Controller/Api/PostController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Category;
+use App\Entity\Locality;
+use App\Entity\Post;
+use App\Entity\User;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;    
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,5 +32,33 @@ class PostController extends AbstractController
         $post = $post->find($id);
 
         return $this->json($post, Response::HTTP_OK, [], ['groups' => 'posts']);
+    }
+
+    #[Route('/post/new', name: 'app_api_post_new', methods: ['POST'])]
+    public function new(EntityManagerInterface $manager, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $categoryId = $data['category'];
+        $category = $manager->getRepository(Category::class)->find($categoryId);
+        $localityId = $data['locality'];
+        $locality = $manager->getRepository(Locality::class)->find($localityId);
+        $userId = $data['user'];
+        $user = $manager->getRepository(User::class)->find($userId);
+
+        $post = new Post();
+        $post->setTitle($data['title']);
+        $post->setResume($data['resume']);
+        $post->setIsActive($data['is_active']);
+        $post->setIsOffer($data['is_offer']);
+        $post->setPublishedAt(new \DateTimeImmutable($data['published_at']));
+        $post->setUpdatedAt(new \DateTimeImmutable($data['updated_at']));
+        $post->setCategory($data['category']);
+        $post->setLocality($data['locality']);
+        $post->setUser($data['user']);
+
+        $manager->persist($post);
+        $manager->flush();
+
+        return $this->json(['message' => 'Create a new post'], Response::HTTP_OK);
     }
 }
