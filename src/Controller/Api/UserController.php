410e3e26b11337f2a2b3507f4c9f98c8adb6c91f
jérémy <jeremy.jqt123@gmail.com>
2023-09-28 15:38:31 +0200

    wip_api_post_user

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
+use App\Entity\Locality;
+use PhpParser\Builder\Method;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/api')]
 class UserController extends AbstractController
@@ -26,4 +32,32 @@ class UserController extends AbstractController
 
         return $this->json($user, Response::HTTP_OK, [], ['groups' => 'users']);
     }
+ //! WIP en attente de refonte de la BDD------------------------------------------------------------------
+    #[Route('/user/new', name: 'app_api_user_new', Methods: ['POST'])]
+    public function new(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $manager): JsonResponse
+    {   
+        $data = json_decode($request->getContent(), true);
+        //$locality = $manager->getRepository(Locality::class)->find($data['locality']);
+        $user = new User;
+        $locality = new Locality;
+       
+        $user->setEmail($data['email']);
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->$passwordHasher->hashPassword($user, $data['password']));
+        $user->setAlias($data['alias']);
+        $user->setFirstname($data['firstname']);
+        $user->setLastname($data['lastname']);
+        $user->setAvatar($data['avatar']);
+        $locality->setZipcode($data['Zipcode']);
+        $locality->setCity($data['City']);
+        $locality->setAdress($data['Adress']);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        return $this->json(['message' => 'Merci de vous être inscrit !'], Response::HTTP_OK);
+    }
+ //! WIP en attente de refonte de la BDD------------------------------------------------------------------
+
+
 }
