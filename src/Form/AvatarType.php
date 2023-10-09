f2352b6c8fbd5616e5f8faf50b7f512305c0abd3
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-10-09 21:24:22 +0200

    add_style_new_templates

--- src/Form/AvatarType.php
+++ src/Form/AvatarType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Avatar;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,12 @@ class AvatarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('url')
-            ->add('name')
+            ->add('url', TextType::class, [
+                'label' => 'URL de l\'avatar',
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'avatar',
+            ])
         ;
     }
 
