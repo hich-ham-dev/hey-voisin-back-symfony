b8c038ad63d69b30f9fec414f1c83a5822c379d0
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-10-12 10:55:28 +0200

    del_choice_type_use

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -8,7 +8,6 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
