e90d6d8f8f2b5d2d86c1a4f4f54cc0be95e329b4
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-10-13 16:26:19 +0200

    wip_coding_voter

--- src/Security/Voter/BackOfficeVoter.php
+++ src/Security/Voter/BackOfficeVoter.php
@@ -16,7 +16,7 @@ class BackOfficeVoter extends Voter
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
         return in_array($attribute, [self::EDIT, self::VIEW])
-            && $subject instanceof \App\Entity\BackOffice;
+            && $subject instanceof \App\Entity\User;
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
@@ -27,16 +27,22 @@ class BackOfficeVoter extends Voter
             return false;
         }
 
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            return true;
+        }
+
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
             case self::EDIT:
                 // logic to determine if the user can EDIT
                 // return true or false
+                return in_array('ROLE_MODERATOR', $user->getRoles());
                 break;
 
             case self::VIEW:
                 // logic to determine if the user can VIEW
                 // return true or false
+                return in_array('ROLE_MODERATOR', $user->getRoles());
                 break;
         }
 
