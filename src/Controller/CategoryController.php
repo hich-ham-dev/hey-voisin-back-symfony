32fcb1adc3041f1560d9f129d996bfe236bf8dce
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-10-16 15:16:52 +0200

    add_restricted_access_avatar

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,8 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/category')]
+#[IsGranted('ROLE_ADMIN')]
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'app_category_index', methods: ['GET'])]
