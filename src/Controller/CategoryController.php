3f466fc877989c8854053727c6378789df147904
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-12-20 16:52:00 +0100

    add comments in CategoryController

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -20,6 +20,7 @@ class CategoryController extends AbstractController
     #[Route('/', name: 'app_category_index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        // Display all categories with pagination
         $pagination = $paginator->paginate(
             $categoryRepository->paginationQuery(),
             $request->query->getInt('page', 1),
@@ -33,10 +34,12 @@ class CategoryController extends AbstractController
     #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // Create a new category with form
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
+        // Verify if form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($category);
             $entityManager->flush();
@@ -61,9 +64,11 @@ class CategoryController extends AbstractController
     #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
     {
+        // Edit a category with form
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
+        // Verify if form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
@@ -79,6 +84,7 @@ class CategoryController extends AbstractController
     #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
     public function delete(Request $request, Category $category, EntityManagerInterface $entityManager): Response
     {
+        // Delete a category
         if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
             $entityManager->remove($category);
             $entityManager->flush();
