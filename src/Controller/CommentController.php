80cec250c252499455c1703c372c6aa91a62f1fa
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-12-20 18:06:42 +0100

    add comments in CommentController

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -20,12 +20,14 @@ class CommentController extends AbstractController
     #[Route('/', name: 'app_comment_index', methods: ['GET'])]
     public function index(CommentRepository $commentRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        // Display all comments with pagination
         $pagination = $paginator->paginate(
             $commentRepository->paginationQuery(),
             $request->query->getInt('page', 1),
             10
         );
 
+        // Render view
         return $this->render('comment/index.html.twig', [
             'pagination' => $pagination,
         ]);
@@ -34,18 +36,22 @@ class CommentController extends AbstractController
     #[Route('/new', name: 'app_comment_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // Create a new comment with form
         $comment = new Comment();
         $comment->setPublishedAt(new \DateTimeImmutable());
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
+        // Verify if form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($comment);
             $entityManager->flush();
 
+            // Redirect to comment index
             return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Render view
         return $this->render('comment/new.html.twig', [
             'comment' => $comment,
             'form' => $form,
@@ -55,6 +61,7 @@ class CommentController extends AbstractController
     #[Route('/{id}', name: 'app_comment_show', methods: ['GET'])]
     public function show(Comment $comment): Response
     {
+        // Display a comment
         return $this->render('comment/show.html.twig', [
             'comment' => $comment,
         ]);
@@ -63,16 +70,20 @@ class CommentController extends AbstractController
     #[Route('/{id}/edit', name: 'app_comment_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
     {
+        // Edit a comment with form
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
+        // Verify if form is submitted and valid
         if ($form->isSubmitted() && $form->isValid()) {
             $comment->setUpdatedAt(new \DateTimeImmutable());
             $entityManager->flush();
 
+            // Redirect to comment index
             return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
         }
 
+        // Render view
         return $this->render('comment/edit.html.twig', [
             'comment' => $comment,
             'form' => $form,
@@ -82,11 +93,13 @@ class CommentController extends AbstractController
     #[Route('/{id}', name: 'app_comment_delete', methods: ['POST'])]
     public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
     {
+        // Delete a comment
         if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
             $entityManager->remove($comment);
             $entityManager->flush();
         }
 
+        // Redirect to comment index
         return $this->redirectToRoute('app_comment_index', [], Response::HTTP_SEE_OTHER);
     }
 }
