e782637f1097cabca902a46643a8fe74b278e273
HichamHamza786 <hicham.hamza.dev@gmail.com>
2023-12-23 22:42:11 +0100

    wip navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",],  
   theme: {
     extend: {},
   },
