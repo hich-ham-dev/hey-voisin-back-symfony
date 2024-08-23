8c59ea011d95b07b74bab07fa7c1ab31133a530d
hich-ham-dev <hicham.hamza.dev@gmail.com>
2024-08-23 14:03:13 +0200

    chore: Update templates to improve responsiveness and styling

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -29,7 +29,7 @@ class PostController extends AbstractController
         );
 
         // Render view
-        return $this->render('post/index2.html.twig', [
+        return $this->render('post/index.html.twig', [
             'pagination' => $pagination,            
         ]);
     }
